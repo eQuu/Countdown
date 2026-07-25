1d62220f707f2a8d854d14a6168904647620c501
Curaw <herrkison@gmail.com>
2026-07-25 22:58:48 +0200

    Game over scene wird immer dreckiger aber hauptsache fertig werden v0.89

--- PlayerCheerScript.cs
+++ PlayerCheerScript.cs
@@ -4,18 +4,36 @@ using System;
 public partial class PlayerCheerScript : CharacterBody3D
 {
     [Export] public float IndicatorRingSize { get; set; } = 1.35f;
-    [Export] public Color IndicatorColor { get; set; } = new Color(0.1f, 0.4f, 1.0f);
+    [Export] private Color _indicatorColor { get; set; } = new Color(0.1f, 0.4f, 1.0f);
+    [Export] private Label3D _winnerLabel;
+    [Export] private Label3D _loserLabel;
     [Export] public bool IsWinner = false;
 
     [Export] private AnimationPlayer _animationPlayer;
     [Export] private MeshInstance3D _indicatorRing;
 
     private ShaderMaterial _indicatorRingMaterial;
+    private int _winnerIndex;
+    private int _loserIndex;
 
     public override void _Ready()
     {
+        _winnerIndex = GameStore.Instance.WinnerPlayerId;
+        _loserIndex = _winnerIndex == 1 ? 2 : 1;
+
+        if (_winnerIndex == 1)
+        {
+            _indicatorColor = GameStore.Instance.GetPlayerColor(1);
+        }
+        else
+        {
+            _indicatorColor = GameStore.Instance.GetPlayerColor(2);
+        }
+
         if (IsWinner)
         {
+            _winnerLabel.Text = GameStore.Instance.GetScore(_winnerIndex).ToString();
+            _loserLabel.Text = GameStore.Instance.GetScore(_loserIndex).ToString();
             _animationPlayer.Play("cheer");
         }
     }
@@ -51,6 +69,6 @@ public partial class PlayerCheerScript : CharacterBody3D
             return;
         }
 
-        _indicatorRingMaterial.SetShaderParameter("ring_color", IndicatorColor);
+        _indicatorRingMaterial.SetShaderParameter("ring_color", _indicatorColor);
     }
 }
