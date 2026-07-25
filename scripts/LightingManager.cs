3a55876cf07a7c80deadfb3c52eb776418d3765e
LaraldHesch <gamejamdulli@gmail.com>
2026-07-25 13:40:46 +0200

    der Museumswärter kann jetzt das Licht einschalten

---
+++ scripts/LightingManager.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+
+namespace Countdown.scripts;
+
+public partial class LightingManager : Node
+{
+    [Export] private Node3D _ceilingLights;
+
+    public void ActivateCeilingLights()
+    {
+        if (_ceilingLights == null) return;
+        _ceilingLights.Visible = true;
+    }
+
+    public void DeactivateCeilingLights()
+    {
+        if (_ceilingLights == null) return;
+        _ceilingLights.Visible = false;
+    }
+}
