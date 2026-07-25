3ec8098130be5bbc6443f369e3b6baf9f36bd933
Curaw <herrkison@gmail.com>
2026-07-25 22:31:38 +0200

    game over screen v0.85

--- PlayerCheerScript.cs
+++ PlayerCheerScript.cs
@@ -3,10 +3,54 @@ using System;
 
 public partial class PlayerCheerScript : CharacterBody3D
 {
-	[Export] private AnimationPlayer _animationPlayer;
+    [Export] public float IndicatorRingSize { get; set; } = 1.35f;
+    [Export] public Color IndicatorColor { get; set; } = new Color(0.1f, 0.4f, 1.0f);
+    [Export] public bool IsWinner = false;
+
+    [Export] private AnimationPlayer _animationPlayer;
+    [Export] private MeshInstance3D _indicatorRing;
+
+    private ShaderMaterial _indicatorRingMaterial;
 
     public override void _Ready()
     {
-        _animationPlayer.Play("cheer");
+        if (IsWinner)
+        {
+            _animationPlayer.Play("cheer");
+        }
+    }
+
+    private void SetupIndicatorRing()
+    {
+        if (_indicatorRing == null)
+        {
+            return;
+        }
+
+        Shader shader = GD.Load<Shader>("res://resources/player/indicator_ring.gdshader");
+        _indicatorRingMaterial = new ShaderMaterial
+        {
+            Shader = shader
+        };
+        _indicatorRing.MaterialOverride = _indicatorRingMaterial;
+        _indicatorRing.CastShadow = GeometryInstance3D.ShadowCastingSetting.Off;
+
+        if (_indicatorRing.Mesh is PlaneMesh planeMesh)
+        {
+            float size = Mathf.Max(0.4f, IndicatorRingSize);
+            planeMesh.Size = new Vector2(size, size);
+        }
+
+        ApplyIndicatorRingColor();
+    }
+
+    private void ApplyIndicatorRingColor()
+    {
+        if (_indicatorRingMaterial == null)
+        {
+            return;
+        }
+
+        _indicatorRingMaterial.SetShaderParameter("ring_color", IndicatorColor);
     }
 }
